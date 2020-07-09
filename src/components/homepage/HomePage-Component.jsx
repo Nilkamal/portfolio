b83a8feef629a5561bf1b1a9dbbf713156d05f7a
Nilkamal Shah <nilkamalsha75@gmail.com>
2020-07-09 16:46:49 +0530

    Update HomePage-Component.jsx

--- src/components/homepage/HomePage-Component.jsx
+++ src/components/homepage/HomePage-Component.jsx
@@ -11,11 +11,11 @@ const HomeContainer = styled.div`
 export default () => {
     return (
         <HomeContainer>
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Nilkamal Shah</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Full Stack Software Engineer</h6>
-                    <p class="card-text">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Nilkamal Shah</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Full Stack Software Engineer</h6>
+                    <p className="card-text">
                         I am software engineer at <em>Publicis Sapient</em> Since Oct, 2019. I am currently loving working with react as eco system and solve interesting challenges.
                         I am currently working with:
                         <h3 style={{marginTop: '1rem'}}>Technologies</h3>
@@ -31,7 +31,7 @@ export default () => {
                     </p>
                     <p>(M)<a href="tel: +91 9913741838">+91 991341818</a></p>
                     <p>(E) <a href="mailto: nilkamalsha75@gmail.com">Nilkamalsha75@gmail.com</a></p>
-                    <a href="https://www.github.com/Nilkamal" target="_blank" class="card-link">View On GitHub</a>
+                    <a href="https://www.github.com/Nilkamal" target="_blank" rel="noopener noreferrer" className="card-link">View On GitHub</a>
                 </div>
             </div>
         </HomeContainer>
