60739f71c3d4e0d81de9c7b3efc98273bdb865d6
Nilkamal Shah <nilkamal.shah@publicissapient.com>
2020-07-09 20:59:02 +0530

    Updating menu and navigations

--- src/providers/menu-providers/Menu-Provider.jsx
+++ src/providers/menu-providers/Menu-Provider.jsx
@@ -1,32 +1,41 @@
 import React, { createContext, useState, useEffect } from 'react';
-
+import { withRouter } from 'react-router-dom';
 export const MenuContext = createContext({
     menuOpen: false, 
     toggleMenu:() => {},
+    goHome: () => {},
     background: '#000',
+    
 });
 
-const MenuProvider = ({children}) => {
+const MenuProvider = ({children, history}) => {
     const [menuOpen, setMenuOpen] = useState(false);
-    const [background, setBackground] = useState('#000');
-
+   
     useEffect(() => {
-        const background = menuOpen ? '#fec33f' : '#000';
-        setBackground(background);
-    },[menuOpen])
+        if (menuOpen) {
+            history.push('/menu');
+        } else {
+            history.push('/');
+        }
+    },[menuOpen, history])
+
     const toggleMenu = () => {
         setMenuOpen(!menuOpen)
+    }
+
+    const goHome = () => {
+        history.push('/');
     }
 
     return (
         <MenuContext.Provider value={{
             toggleMenu, 
-            menuOpen,
-            background
+            menuOpen, 
+            goHome
         }}>
             {children}
         </MenuContext.Provider>
     )
 }
 
-export default MenuProvider;
+export default withRouter(MenuProvider);
